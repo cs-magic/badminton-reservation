6325d15c4a484e608e7bf884abb15b79769c3e48
markshawn2020 <shawninjuly@gmail.com>
2024-11-17 15:39:22 +0800

    added cases/semantic-pdf-parser

--- app/getBookingData.tsx
+++ app/getBookingData.tsx
@@ -1,18 +1,17 @@
-'use server';
+"use server"
 
-import { api } from "@cs-magic/common/dist/api/api.js";
-import { BookingSlot } from "../types";
+import { api } from "@cs-magic/common/api/api.js"
+
+import { BookingSlot } from "../types"
 
 export async function getBookingData(date: string, gym: string): Promise<BookingSlot[]> {
-    const data = await api.get(
-        `https://api.cs-magic.cn/badminton?date=${date}&gym=${gym}`
-    );
-    const result = data.data.map((item: any) => ({ 
-        start_time: item.start_time,
-        end_time: item.end_time,
-        place: gym,
-        available_count: item.available_count
-    }));
-    console.log("API called for", gym, "on", date);
-    return result;
+  const data = await api.get(`https://api.cs-magic.cn/badminton?date=${date}&gym=${gym}`)
+  const result = data.data.map((item: any) => ({
+    start_time: item.start_time,
+    end_time: item.end_time,
+    place: gym,
+    available_count: item.available_count,
+  }))
+  console.log("API called for", gym, "on", date)
+  return result
 }
